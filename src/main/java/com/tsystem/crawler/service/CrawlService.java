223d7ba11a18790917de6ac454f38a3134744bae
gauravc65 <41051183+gauravc65@users.noreply.github.com>
2020-02-16 22:35:53 +0530

    Update CrawlService.java

--- src/main/java/com/tsystem/crawler/service/CrawlService.java
+++ src/main/java/com/tsystem/crawler/service/CrawlService.java
@@ -15,9 +15,7 @@ import java.util.regex.Pattern;
 @Service
 public class CrawlService {
 
-    private static final int MAX_PAGES_TO_SEARCH = 10;
     private Set<String> pagesVisited = new HashSet<String>();
-    private Set<String> pagesToVisit = new HashSet<String>();
     private static final String USER_AGENT =
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
     private static final Pattern filters = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g"
